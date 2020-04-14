f610562d095f7d6232305a04ab3c2188150d87f4
Mathew Jackson <tutters2@hotmail.com>
2020-04-14 16:09:58 +0100

    added blog/port pages with hide function

--- js/hide.js
+++ js/hide.js
@@ -1,14 +1,29 @@
 function hide(DIV) {
-    let div = document.getElementById(DIV);
-    let frontEnd = 'closed'
+  let div = document.getElementById(DIV);
+
+  if (div === blog) {
+    port.style.display = "none";
+  }
+
+  if (div === port) {
+    blog.style.display = "none";
+  }
 
 
-        if (div.style.display === "none") {
-        div.style.display = "block";
-        // div.scrollIntoView();
-        }
-       else if (div.id !== 'Front-End-Div') {
-         div.style.display = "none";
-        }
-    
+  if (div.style.display === "none") {
+    div.style.display = "block";
+    // div.scrollIntoView();
+  } else if (div.id !== 'Front-End-Div') {
+    div.style.display = "none";
+  }
+
 }
+
+// function hide(id) {
+//   var x = document.getElementById("id");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
