9c70a321402503d8c32557dc3ce84f187ff8ece5
Mathew Jackson <tutters2@hotmail.com>
2020-04-22 14:24:27 +0100

    added blog object and for loop using i as an id to build multiple blogs

--- js/blog.js
+++ js/blog.js
@@ -1,56 +1,83 @@
+let blog = [{
+  mainID: "blog1",
+  innerID: "inner1",
+  titleID: "title1",
+  dateID: "date1",
+  contentID: "content1",
+  imageID: "image1",
+  title: "object title",
+  date: "10/11/2044",
+  content: "blog object content",
+  imageSRC: "Images/antdentifierex.png",
+}, {
+  mainID: "blog2",
+  innerID: "inner2",
+  titleID: "title2",
+  dateID: "date2",
+  contentID: "content2",
+  imageID: "image2",
+  title: "object title 2",
+  date: "10/11/2044 2",
+  content: "blog object content 2",
+  imageSRC: "Images/antdentifierex.png",
+}];
+
+
 function createBlog() {
-  // Blog Main
-  var mainDiv = document.createElement("DIV");
-  document.body.appendChild(mainDiv);
-  mainDiv.setAttribute("id", "blog");
-  mainDiv.setAttribute("class", "col-12 animated bounceInUp blogMain")
 
-  // Inner div
-  var innerMain = document.createElement("DIV");
-  innerMain.setAttribute("id", "innerBlog");
-  innerMain.setAttribute("class", "blogPort col-12 row");
-  document.getElementById("blog").appendChild(innerMain);
+  for (var i = 0; i < blog.length; i++) {
 
-  // Title div and title
-  var titleDiv = document.createElement("DIV");
-  titleDiv.setAttribute("id", "titleDiv");
-  titleDiv.setAttribute("class", "col-6");
-  document.getElementById("innerBlog").appendChild(titleDiv);
+    // Blog Main
+    var mainDiv = document.createElement("DIV");
+    document.body.appendChild(mainDiv);
+    mainDiv.setAttribute("id", "blogID" + i);
+    mainDiv.setAttribute("class", "col-12 animated bounceInUp blogMain");
 
-  var title = document.createElement("H4");
-  title.innerHTML = "This is a title.";
-  document.getElementById("titleDiv").appendChild(title);
+    // Inner div
+    var innerMain = document.createElement("DIV");
+    innerMain.setAttribute("id", "innerID" + i);
+    innerMain.setAttribute("class", "blogPort col-12 row");
+    document.getElementById("blogID" + i).appendChild(innerMain);
 
-  //Date Div and date
-  var dateDiv = document.createElement("DIV");
-  dateDiv.setAttribute("id", "dateDiv");
-  dateDiv.setAttribute("class", "col-6 text-right");
-  document.getElementById("innerBlog").appendChild(dateDiv);
+    // Title div and title
+    var titleDiv = document.createElement("DIV");
+    titleDiv.setAttribute("id", "titleID" + i);
+    titleDiv.setAttribute("class", "col-6");
+    document.getElementById("innerID" + i).appendChild(titleDiv);
 
-  var date = document.createElement("H4");
-  date.innerHTML = "26/10/1987";
-  document.getElementById("dateDiv").appendChild(date);
+    var title = document.createElement("H4");
+    title.innerHTML = blog[i].title;
+    document.getElementById("titleID" + i).appendChild(title);
 
-  //content Div and content
-  var contentDiv = document.createElement("DIV");
-  contentDiv.setAttribute("id", "contentDiv");
-  contentDiv.setAttribute("class", "col-12 text-center");
-  document.getElementById("innerBlog").appendChild(contentDiv);
+    //Date Div and date
+    var dateDiv = document.createElement("DIV");
+    dateDiv.setAttribute("id", "dateID" + i);
+    dateDiv.setAttribute("class", "col-6 text-right");
+    document.getElementById("innerID" + i).appendChild(dateDiv);
 
-  var content = document.createElement("P");
-  content.innerHTML =
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-  document.getElementById("contentDiv").appendChild(content);
+    var date = document.createElement("H4");
+    date.innerHTML = blog[i].date;
+    document.getElementById("dateID" + i).appendChild(date);
 
-  //Image Div and Image
-  var imageDiv = document.createElement("DIV");
-  imageDiv.setAttribute("id", "imageDiv");
-  imageDiv.setAttribute("class", "col-12 text-center");
-  document.getElementById("innerBlog").appendChild(imageDiv);
+    //content Div and content
+    var contentDiv = document.createElement("DIV");
+    contentDiv.setAttribute("id", "contentID" + i);
+    contentDiv.setAttribute("class", "col-12 text-center");
+    document.getElementById("innerID" + i).appendChild(contentDiv);
 
-  var image = document.createElement("IMG");
-  image.setAttribute("src", "Images/antdentifierex.png");
-  image.setAttribute("class", "mx-auto d-block img-fluid");
-  document.getElementById("imageDiv").appendChild(image);
+    var content = document.createElement("P");
+    content.innerHTML = blog[i].content;
+    document.getElementById("contentID" + i).appendChild(content);
 
+    //Image Div and Image
+    var imageDiv = document.createElement("DIV");
+    imageDiv.setAttribute("id", "imageID" + i);
+    imageDiv.setAttribute("class", "col-12 text-center");
+    document.getElementById("innerID" + i).appendChild(imageDiv);
+
+    var image = document.createElement("IMG");
+    image.setAttribute("src", blog[i].imageSRC);
+    image.setAttribute("class", "mx-auto d-block img-fluid");
+    document.getElementById("imageID" + i).appendChild(image);
+  }
 }
